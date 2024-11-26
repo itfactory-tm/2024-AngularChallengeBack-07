7b943c3e0d1841b6eabc6ec2a55e674b0acb3a67
Seppe Van Campfort <r0929446@student.thomasmore.be>
2024-11-26 09:59:37 +0100

    updated docker compose and file

--- FritFest.API/FritFest.API/Program.cs
+++ FritFest.API/FritFest.API/Program.cs
@@ -17,7 +17,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.WebHost.UseUrls("http://0.0.0.0:80");
 //// Configure Auth0 Authentication
 builder.Services.AddAuthentication(options =>
 {
